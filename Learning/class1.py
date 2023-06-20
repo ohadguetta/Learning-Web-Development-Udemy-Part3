6a4bd912bb80db72c36fac286bfff2f20a08ebc7
ohadguetta <o4255542@gmail.com>
2023-06-20 09:37:14 +0300

    learning python

--- Learning/class1.py
+++ Learning/class1.py
@@ -1,7 +1,17 @@
-lst = ["hello", "world"]
-for i in lst:
-    print(i)
-lst = list(range(1, 11))
-print(lst)
-if lst == list(range(1, 11)):
-    print(True)
+fireworks_types = ("Sparklers", "Fountains", "Roman Candles", "Firecrackers", "Smoke Bombs", "Aerial Shells", "Mines", "Cake Batteries", "Bottle Rockets", "Ground Spinners")
+for fType in fireworks_types:
+    print('firework type:',fType)
+
+lst = ['hello','world']
+dict = {
+     'name':'ohad',
+     'age':20,
+    }
+print('hello my name is:',dict['name'])
+
+def hello():
+    print(10)
+
+print(hello())
+#tuple1 = ('hello','world')
+#set1 = {'hello','world'}
