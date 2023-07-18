adebd965ee4787ebe30f6985cf088261257ed095
ohadguetta <o4255542@gmail.com>
2023-07-19 00:12:28 +0300

    added image field

--- Django/django101/feed/views.py
+++ Django/django101/feed/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import TemplateView
 
+from .models import Post
+
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["this_thing"] = str(list(range(1,11)))
+        context['posts'] = Post.objects.all()
         return context
     
